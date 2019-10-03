8e4e4b9be8e8fc39077fe2177000079d7cd62913
hala <hala.freijat@outlook.com>
2019-10-03 12:51:27 +0300

    Javascipt modify

--- shopp2.js
+++ shopp2.js
@@ -1,18 +1,19 @@
 // 'use strict'
-var hala = function() {
+function hala() {
     var b;
     var a = prompt('choose a number');
     
-      while( a !== '1' && a !== '0') {
-          a = promit ('please pick 1 or 0');
+      while( a !== 1 && a !== 0) {
+          prompt('please pick 1 or 0');
       }
-          if (a == '1') {
+
+          if (a === 1) {
           b = 'win';
           }
-          else if( a == '0') {
+          else if( a === 0) {
           b = 'lose';
           }
-     
+      
   return b;
   }
   
